6c27f34ea01e216dc1b70577d0925605412515c3
Tim <tmeier42+github@gmail.com>
2019-04-26 23:44:21 +0200

    manifest has ver bump because of bad readme title

---
+++ README.md
@@ -0,0 +1,13 @@
+# UnlimitedHeadstomps
+
+Risk of Rain 2 mod to remove the cooldown on the Headstompers / H3AD-5T v2
+
+ - H3AD-5T v2 by default reduces its cooldown by half for each stack of the item.
+ - It's rare to get more than one stack of this item, and the damage output is very low considering how they have to be used.
+ - Therefore, it doesn't seem imbalanced to remove the cooldown entirely.
+
+
+### Installation
+
+- Requires BepInEx
+- Copy the dll to the BepinEx plugins folder
---
+++ UnlimitedHeadstomps/UnlimitedHeadstomps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BepInEx;
+using System.Reflection;
+using System.Linq;
+
+namespace UnlimitedHeadstomps
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.Timmeh42.UnlimitedHeadstomps", "UnlimitedHeadstomps", "1.0.0")]
+    public class UnlimitedHeadstomps : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.EntityStates.Headstompers.HeadstompersCooldown.OnEnter += (orig, self) =>
+            {
+                Assembly assembly = self.GetType().Assembly;
+                Type HSCooldown = assembly.GetTypes().First(t => t.IsClass && t.Namespace == "EntityStates.Headstompers" && t.Name == "HeadstompersCooldown");
+
+                HSCooldown.GetField("baseDuration", BindingFlags.Static | BindingFlags.Public).SetValue(null, 0f);
+                orig(self);
+            };
+        }
+    }
+}
